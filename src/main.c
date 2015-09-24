82eb517cb13bf9ea63b1856de91fb74f1cd0635a
davidfialho <fialho.david@gmail.com>
2015-09-24 19:30:34 +0100

    added test for the front isertion and reverse iteration

--- src/main.c
+++ src/main.c
@@ -12,7 +12,20 @@ int main() {
         List_insertBack(list, number);
     }
 
+    printf("FIRST PRINT\n");
     for(ListIterator it = List_begin(list); it != List_end(list); it = List_next(it)) {
+        int* number = List_item(it);
+        printf("%d\n", *number);
+    }
+
+    for(int i = 0; i < 10; i++) {
+        int* number = malloc(sizeof(int));
+        *number = i;
+        List_insertFront(list, number);
+    }
+
+    printf("SECOND PRINT\n");
+    for(ListIterator it = List_reverseBegin(list); it != List_reverseEnd(list); it = List_previous(it)) {
         int* number = List_item(it);
         printf("%d\n", *number);
     }
