bf63e61f68911b2a67d79b17b2ecb6d9b41a2e9a
davidfialho <fialho.david@gmail.com>
2015-09-24 18:22:21 +0100

    defined a new data type List defined the interface for the list

---
+++ src/containers/list.h
@@ -0,0 +1,23 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct List List;
+typedef void* Item;
+
+List* List_new();
+void List_delete(List* list, void (*delete)(void*));
+
+void List_insertFront(List* list, Item item);
+void List_insertBack(List* list, Item item);
+void List_removeFront(List* list);
+void List_removeBack(List* list);
+
+Item List_front(List* list);
+Item List_back(List* list);
+size_t List_size(List* list);
+bool List_isEmpty(List* list);
+
+#endif //LIST_H
