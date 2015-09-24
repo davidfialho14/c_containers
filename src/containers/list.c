adb6b6e44cc23156b1b25c52a47547d253e3d64b
davidfialho <fialho.david@gmail.com>
2015-09-24 18:32:22 +0100

    implemented the constructor and destructor of the list

--- src/containers/list.c
+++ src/containers/list.c
@@ -1,2 +1,46 @@
+#include <malloc.h>
 #include "list.h"
 
+typedef struct Element {
+    Item item;
+    struct Element* next;
+} Element;
+
+struct List {
+    Element* front;
+    Element* back;
+    size_t size;
+};
+
+List* List_new() {
+    // create list structure
+    List* list = malloc(sizeof(List));
+    // setup empty list
+    list->front = list->back = NULL;
+    list->size = 0;
+
+    return list;
+}
+
+void List_delete(List* list, void (*delete)(void*)) {
+
+    if(delete == NULL) {
+        // use the default free function to delete the items
+        delete = free;
+    }
+
+    Element* aux = list->front;
+    // remove each element in the list
+    while(aux != NULL) {
+        // move front of the list to the next element
+        list->front = aux->next;
+
+        // delete the item
+        delete(aux->item);
+        // delete the element
+        free(aux);
+
+        // move to next element pointed by the fron pointer
+        aux = list->front;
+    }
+}
