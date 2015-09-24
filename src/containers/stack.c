2a523f4893dc339f819adf36c9351a26ddc1a52f
davidfialho <fialho.david@gmail.com>
2015-09-24 20:56:12 +0100

    implemented the stack based on the list container

---
+++ src/containers/stack.c
@@ -0,0 +1,41 @@
+#include <malloc.h>
+#include "stack.h"
+
+Stack* Stack_new() {
+    return List_new();
+}
+
+void Stack_delete(Stack* stack, void (* delete)(void*)) {
+    return List_delete(stack, NULL);
+}
+
+void Stack_add(Stack* stack, Item item) {
+    List_insertFront(stack, item);
+}
+
+Item Stack_get(Stack* stack, Item item) {
+    return List_removeFront(stack);
+}
+
+Item Stack_peek(Stack* stack, Item item) {
+    return List_front(stack);
+}
+
+void Stack_pop(Stack* stack, void (* delete)(void*)) {
+
+    if(delete == NULL) {
+        // use as default the free function
+        delete = free;
+    }
+
+    Item item = List_removeFront(stack);
+    delete(item);
+}
+
+size_t Stack_size(const Stack* stack) {
+    return List_size(stack);
+}
+
+bool Stack_isEmpty(const Stack* stack) {
+    return List_isEmpty(stack);
+}
